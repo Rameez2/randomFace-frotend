509811c12dc75bcecd0eced1ac45978e29f3950c
Rameez2 <syedrameezshahpesh@gmail.com>
2024-09-11 08:04:57 -0700

    change from wss to https again

--- src/socket.js
+++ src/socket.js
@@ -10,10 +10,8 @@ import { io } from 'socket.io-client';
 // };
 
 export const getSocket = () => {
-  let socket = io('wss://random-face-backend.vercel.app', {
-    transports: ['websocket'],
-  }); 
+  // let socket = io('wss://random-face-backend.vercel.app'}); // URL of your server
   // let socket = io('http://localhost:3001'); // URL of your server
-  // let socket = io('https://random-face-backend.vercel.app'); // URL of your server
+  let socket = io('https://random-face-backend.vercel.app'); // URL of your server
   return socket;
 };
